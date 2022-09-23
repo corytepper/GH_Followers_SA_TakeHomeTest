d1f27197877b418a110cf0090e1c4f7f9b7250d1
Cory Tepper <corytepper@gmail.com>
2022-09-23 09:12:14 -0400

    Initial Commit

---
+++ GH_Followers_SA_TakeHomeTest/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GH_Followers_SA_TakeHomeTest
+//
+//  Created by Cory Tepper on 8/21/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+       
+    }
+    
+}
