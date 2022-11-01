9feb16d1fcecc1cd646be6ad75512744451b6749
Cory Tepper <corytepper@gmail.com>
2022-11-01 19:27:16 -0400

    Result Type refactor

--- GH_Followers_SA_TakeHomeTest/Utilities/ErrorMessage.swift
+++ GH_Followers_SA_TakeHomeTest/Utilities/ErrorMessage.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-enum ErrorMessage: String {
+enum GFError: String, Error {
     case invalidUsername    = "This username created an invalid request. Please try again."
-    case unabaleToComplete  = "Unable to complete your request. Please check your internet connection"
+    case unableToComplete  = "Unable to complete your request. Please check your internet connection"
     case invalidResponse    = "Invalid response from the server. Please try again."
     case invalidData        = "The data recieved from the server was invalid. Please try again."
 }
