f38abc3654168ced17527313bc8e4500ff771062
Cory Tepper <corytepper@gmail.com>
2022-11-02 12:24:36 -0400

    UICollectionView - ARC, Memory Leaks, and Capture Lists [weak self] // Downloading & Caching Avatar Images

--- GH_Followers_SA_TakeHomeTest/Managers/NetworkManager.swift
+++ GH_Followers_SA_TakeHomeTest/Managers/NetworkManager.swift
@@ -5,11 +5,13 @@
 //  Created by Cory Tepper on 9/29/22.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared   = NetworkManager()
-    let baseURL         = "https://api.github.com/users/"
+    private let baseURL         = "https://api.github.com/users/"
+    let cache           = NSCache<NSString, UIImage>()
+    
     
     private init() {}
     
