92da09d146659c02b0d36e7eb64a218f592658e6
Cory Tepper <coryteppe@gmail.com>
2022-11-07 09:34:49 -0500

    Button Functionality - Delegates & Protocols

--- GH_Followers_SA_TakeHomeTest/Custom Views/ViewControllers/IItemInfoVCs/GFFollowerItemVC.swift
+++ GH_Followers_SA_TakeHomeTest/Custom Views/ViewControllers/IItemInfoVCs/GFFollowerItemVC.swift
@@ -18,5 +18,11 @@ class GFFollowerItemVC: GFItemInfoVC {
         itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
         itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
         actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
-    }    
+    }
+    
+    override func actionButtonTapped() {
+        delegate.didTapGetFollowers(for: user)
+    }
 }
+
+
--- GH_Followers_SA_TakeHomeTest/Screens/FollowerListVC.swift
+++ GH_Followers_SA_TakeHomeTest/Screens/FollowerListVC.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol FollowerListVCDelegate: AnyObject {
+    func didRequestFollowers(for username: String)
+}
+
 class FollowerListVC: UIViewController {
     
     enum Section {
@@ -133,6 +137,7 @@ extension FollowerListVC: UICollectionViewDelegate {
         
         let destVC          = UserInfoVC()
         destVC.username     = follower.login
+        destVC.delegate     = self
         let navController   = UINavigationController(rootViewController: destVC)
         present(navController, animated: true)
     }
@@ -153,5 +158,20 @@ extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
         updateData(on: followers)
         
     }
+    
+}
+
+extension FollowerListVC: FollowerListVCDelegate {
+    
+    func didRequestFollowers(for username: String) {
+        self.username   = username
+        title           = username
+        page            = 1
+        followers.removeAll()
+        filteredFollowers.removeAll()
+        collectionView.setContentOffset(.zero, animated: true)
+        getFollowers(username: username, page: page)
+    }
+    
     
 }
